8ff654f819a9058aea0b0e3dfad77a39e9db157e
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2026-05-04 10:36:38 +0100

    multi-test: Move myPackageA into a package dir; add Haddock docs

--- tests/integration/tests/multi-test/Main.hs
+++ tests/integration/tests/multi-test/Main.hs
@@ -1,3 +1,20 @@
+-- | The test's project has project packages A, B and C (which has no library).
+--
+-- In terms of main libraries, the dependencies are (->- is 'depends on'):
+--
+--     A ->- B
+--
+-- In terms of executables (including test suites):
+--
+--     B ->- A and C ->- A
+--
+-- As, overall, A ->- B and B ->- A, packages A and B cannot be built
+-- 'all-in-one'.
+--
+-- This integration test passes when A is named myPackageA and B is named
+-- 0myPackageB, but it fails when B is renamed myPackageB. That must be a bug in
+-- Stack.
+
 import           Control.Monad ( unless )
 import           Data.List ( isInfixOf )
 import           StackTest
